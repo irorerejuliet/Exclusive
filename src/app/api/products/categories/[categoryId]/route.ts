86b61787cbff917f0d2c7756106aba016da8d018
irorere juliet <irorerejuliet@gmail.com>
2026-05-14 22:48:46 +0100

    fetchind product by category

--- src/app/api/products/categories/[categoryId]/route.ts
+++ src/app/api/products/categories/[categoryId]/route.ts
@@ -1,81 +1,66 @@
 
-import { createClient } from "@/lib/supabase/server"
-import { apiResponse } from "@/utils/errorHandling"
+// import { createClient } from "@/lib/supabase/server"
+// import { apiResponse } from "@/utils/errorHandling"
+
+// interface Params {
+//   params: Promise<{ categoryId: string }>;
+// }
+
+// export const GET = async (request: Request, { params }: Params) => {
+//   const supabase = await createClient();
+
+//   const { categoryId } = await params;
+
+//   const { data, error } = await supabase
+//     .from("products")
+//     .select("*")
+//     .eq("category_id", categoryId);
+
+//   if (error) {
+//     return apiResponse({
+//       success: false,
+//       message: error.message,
+//       status: 400,
+//     });
+//   }
+
+//   return apiResponse({
+//     success: true,
+//     message: "Products fetched successfully",
+//     data,
+//     status: 200,
+//   });
+// };
 
 
-
-
-
-interface Params{
-    params: Promise<{categoryId: string}>
-}
-
-interface Params {
-  params: Promise<{ categoryId: string }>;
-}
-
-export const GET = async (request: Request, { params }: Params) => {
-  const supabase = await createClient();
-  const { categoryId } = await params;
-
-  try {
-    const { data, error } = await supabase
-      .from("products")
-      .select("*")
-      .eq("category_id", categoryId);
-
-    if (error) {
-      return apiResponse({
-        success: false,
-        message: error.message,
-        status: 400,
-      });
-    }
-
-    return apiResponse({
-      success: true,
-      message: "Products fetched successfully",
-      data,
-      status: 200,
-    });
-  } catch (error) {
-    return apiResponse({
-      success: false,
-      message: error instanceof Error ? error.message : "Unknown error",
-      status: 500,
-    });
-  }
-};
-
-
-export const DELETE = async (request: Request, {params}: Params) => {
-  const supabase = await createClient()
-  const {categoryId} = await params;
+// export const DELETE = async (request: Request, {params}: Params) => {
+//   const supabase = await createClient()
+//   const {categoryId} = await params;
 
   
-try {
-  const { error } = await supabase
-    .from("categories")
-    .delete()
-    .eq("id", categoryId);
+// try {
+//   const { error } = await supabase
+//     .from("categories")
+//     .delete()
+//     .eq("id", categoryId);
   
-    if(error){
-      return apiResponse({
-        success: false,
-        message: error.message,
-        status: 400,
-      });
-    }
+//     if(error){
+//       return apiResponse({
+//         success: false,
+//         message: error.message,
+//         status: 400,
+//       });
+//     }
 
 
-    return apiResponse({
-      success: true,
-      message: `${categoryId}  delected successfully`,
-    });
-} catch (error) {
-  return apiResponse({
-    success: false,
-    message: error instanceof Error ? error.message : " Something whent wrong",
-  });
-}
-}
+//     return apiResponse({
+//       success: true,
+//       message: `${categoryId}  delected successfully`,
+//     });
+// } catch (error) {
+//   return apiResponse({
+//     success: false,
+//     message: error instanceof Error ? error.message : " Something whent wrong",
+//   });
+// }
+// }
